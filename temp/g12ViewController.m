35291a3660890aa63d48a0de2a2186c2bedd1a6d
Anton Plebanovich <LoWeLy@Antons-Mac-Pro.local>
2014-08-22 15:51:31 +0300

    Change from CALayer to UIView and from .position to CGAffineTransformMakeTranslation for moving square

--- temp/g12ViewController.m
+++ temp/g12ViewController.m
@@ -13,8 +13,8 @@
     double speedX, speedY, addSpeed, maxSpeed, currentSpeed, minAxisSpeed; //Points per second
     double angle;
     CALayer *speedAndAngleDisplay;
-    CALayer *moveSpace;
-    CALayer *movingSquare;
+    UIView *moveSpace;
+    UIView *movingSquare;
     CALayer *dot;
     float displayerSize;
     float radius;
@@ -27,43 +27,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    __block BOOL dotMoveToDefaultPosition = NO;
+    displayerSize = 100;
+    radius = 5;
+    double size = 20.0;
+    angle = arc4random()%360;
 	// Do any additional setup after loading the view, typically from a nib..
-    CABasicAnimation *noAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+//    CABasicAnimation *noAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
     
-    moveSpace = [CALayer layer];
-    movingSquare = [CALayer layer];
+    moveSpace = [[UIView alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, self.view.bounds.size.height - 20 - displayerSize)];
+    movingSquare = [[UIView alloc] initWithFrame:CGRectMake(0, 0, size, size)];
     dot = [CALayer layer];
     speedAndAngleDisplay = [CALayer layer];
     
     [speedAndAngleDisplay addSublayer:dot];
-    [moveSpace addSublayer:movingSquare];
+    [moveSpace addSubview:movingSquare];
     
 //    speedAndAngleDisplay.borderColor = [UIColor blackColor].CGColor;
 //    speedAndAngleDisplay.borderWidth = 1.0;
-    displayerSize = 100;
-    radius = 5;
+    
     [dot setMasksToBounds:YES];
     [dot setCornerRadius:radius];
     dot.frame = CGRectMake(displayerSize / 2 - radius, displayerSize / 2 - radius, radius * 2, radius * 2);
     speedAndAngleDisplay.frame = CGRectMake(self.view.bounds.size.width - displayerSize, self.view.bounds.size.height - displayerSize, displayerSize, displayerSize);
     speedAndAngleDisplay.borderColor = [UIColor blackColor].CGColor;
     speedAndAngleDisplay.borderWidth = 1.0;
-    moveSpace.frame = CGRectMake(0, 20, self.view.bounds.size.width, self.view.bounds.size.height - 20 - displayerSize);
-    moveSpace.borderColor = [UIColor blackColor].CGColor;
-    moveSpace.borderWidth = 1.0;
+    moveSpace.layer.borderColor = [UIColor blackColor].CGColor;
+    moveSpace.layer.borderWidth = 1.0;
     
     dot.backgroundColor = [UIColor blackColor].CGColor;
-    double size = 20.0;
-    angle = arc4random()%360;
-    movingSquare.backgroundColor = [UIColor blackColor].CGColor;
     
-    [self.view.layer addSublayer:moveSpace];
+    movingSquare.backgroundColor = [UIColor blackColor];
+    
+    [self.view addSubview:moveSpace];
     [self.view.layer addSublayer:speedAndAngleDisplay];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         double x = moveSpace.frame.size.width / 2 - size / 2;
         double y = moveSpace.frame.size.height / 2 - size / 2;
-        movingSquare.frame = CGRectMake(0, 0, size, size);
-        movingSquare.position = CGPointMake(x, y);
+        [movingSquare setTransform:CGAffineTransformMakeTranslation(x, y)];
         addSpeed = 100.0;
         minAxisSpeed = 30.0;
         currentSpeed = addSpeed;
@@ -72,6 +73,8 @@
         NSTimeInterval currentTime;
         speedX = addSpeed * cos(M_PI/180.0*angle);
         speedY = addSpeed * sin(M_PI/180.0*angle);
+        if (fabs(speedX) < minAxisSpeed) speedX = speedX / speedX * minAxisSpeed;
+        if (fabs(speedY) < minAxisSpeed) speedX = speedY / speedY * minAxisSpeed;
         addSpeed = 0;
         _currentSpeedLabel.text = [NSString stringWithFormat:@"Current speed: %.3f",currentSpeed];
         _maxSpeedLabel.text = [NSString stringWithFormat:@"Max speed: %.3f",maxSpeed];
@@ -80,72 +83,75 @@
             x += (currentTime - startTime) * speedX;
             y += (currentTime - startTime) * speedY;
             startTime = currentTime;
-            if (x > moveSpace.frame.size.width - size / 2) {
-                x = moveSpace.frame.size.width - size / 2;
+            if (x > moveSpace.frame.size.width - size) {
+                x = moveSpace.frame.size.width - size;
                 speedX = fabs(speedX) * -0.9;
                 speedX += addSpeed * cos(M_PI/180.0*angle);
                 if (speedX > -minAxisSpeed) speedX = -minAxisSpeed;
 //                speedY += addSpeed * sin(M_PI/180.0*angle);
                 addSpeed = 0;
-                dot.position = CGPointMake(displayerSize / 2, displayerSize / 2);
+                dotMoveToDefaultPosition = YES;
             }
-            if (y > moveSpace.frame.size.height - size / 2) {
-                y = moveSpace.frame.size.height - size / 2;
+            if (y > moveSpace.frame.size.height - size) {
+                y = moveSpace.frame.size.height - size;
                 speedY = fabs(speedY) * -0.9;
 //                speedX += addSpeed * cos(M_PI/180.0*angle);
                 speedY += addSpeed * sin(M_PI/180.0*angle);
                 if (speedY > -minAxisSpeed) speedY = -minAxisSpeed;
                 addSpeed = 0;
-                dot.position = CGPointMake(displayerSize / 2, displayerSize / 2);
+                dotMoveToDefaultPosition = YES;
             }
-            if (x < size / 2) {
-                x = size / 2;
+            if (x < 0) {
+                x = 0;
                 speedX = fabs(speedX) * 0.9;
                 speedX += addSpeed * cos(M_PI/180.0*angle);
                 if (speedX < minAxisSpeed) speedX = minAxisSpeed;
 //                speedY += addSpeed * sin(M_PI/180.0*angle);
                 addSpeed = 0;
-                dot.position = CGPointMake(displayerSize / 2, displayerSize / 2);
+                dotMoveToDefaultPosition = YES;
             }
-            if (y < size / 2) {
-                y = size / 2;
+            if (y < 0) {
+                y = 0;
                 speedY = fabs(speedY) * 0.9;
 //                speedX += addSpeed * cos(M_PI/180.0*angle);
                 speedY += addSpeed * sin(M_PI/180.0*angle);
                 if (speedY < minAxisSpeed) speedY = minAxisSpeed;
                 addSpeed = 0;
-                dot.position = CGPointMake(displayerSize / 2, displayerSize / 2);
+                dotMoveToDefaultPosition = YES;
             }
             currentSpeed = sqrt(speedX*speedX + speedY*speedY);
             if (currentSpeed > maxSpeed) {
                 maxSpeed = currentSpeed;
             }
-            if (!movingSquare.needsDisplay && ![movingSquare animationForKey:@"position"]) {
+            if (!movingSquare.layer.needsDisplay) {
                 dispatch_sync(dispatch_get_main_queue(),^{
                     
-                    
-                    CGPoint point = CGPointMake(x, y);
-                    noAnimation.fromValue = [movingSquare valueForKey:@"position"];
-                    noAnimation.toValue = [NSValue valueWithCGPoint:point];
-                    noAnimation.duration = 0.1;
-                    noAnimation.fillMode = kCAFillModeForwards;
-                    movingSquare.position = CGPointMake(x, y);
-                    [movingSquare addAnimation:noAnimation forKey:@"position"];
+                    [movingSquare setTransform:CGAffineTransformMakeTranslation(x, y)];
+
+//                    CGPoint point = CGPointMake(x, y);
+//                    noAnimation.fromValue = [movingSquare valueForKey:@"position"];
+//                    noAnimation.toValue = [NSValue valueWithCGPoint:point];
+//                    noAnimation.duration = 0.05;
+//                    noAnimation.fillMode = kCAFillModeForwards;
+//                    movingSquare.layer.position = CGPointMake(x, y);
+//                    [movingSquare.layer addAnimation:noAnimation forKey:@"position"];
                     
                     [movingSquare setNeedsDisplay];
-                    //                    [movingSquare removeAllAnimations];
-                    
+//                    [movingSquare.layer removeAllAnimations];
                     if (!dot.needsDisplay) {
                         //                    [dot removeAllAnimations];
+                        if (dotMoveToDefaultPosition) {
+                            dot.position = CGPointMake(displayerSize / 2, displayerSize / 2);
+                            dotMoveToDefaultPosition = NO;
+                        }
                         [dot setNeedsDisplay];
+                        
                     }
                     _currentSpeedLabel.text = [NSString stringWithFormat:@"Current speed: %.3f",currentSpeed];
                     _maxSpeedLabel.text = [NSString stringWithFormat:@"Max speed: %.3f",maxSpeed];
                 });
-            } else {
-                usleep(100000);
-                [movingSquare removeAllAnimations];
             }
+            usleep(30000);
         }
     });
 }
@@ -158,10 +164,12 @@
 
 - (IBAction)arc4randomPunch:(id)sender {
     
-    addSpeed = arc4random()%100;
+    addSpeed = arc4random()%(100 - lrint(radius));
     angle = arc4random()%360;
-    dot.frame = CGRectMake(displayerSize / 2 - radius + addSpeed * cos(M_PI/180.0*angle) / 2, displayerSize / 2 - radius + addSpeed * sin(M_PI/180.0*angle) / 2, radius * 2, radius * 2);
-    [dot setNeedsDisplay];
+    dispatch_async(dispatch_get_main_queue(),^{
+        dot.position = CGPointMake(displayerSize / 2 + addSpeed * cos(M_PI/180.0*angle) / 2, displayerSize / 2 + addSpeed * sin(M_PI/180.0*angle) / 2);
+        [dot setNeedsDisplay];
+    });
 }
 
 @end
